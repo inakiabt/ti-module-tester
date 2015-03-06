046bc80b8164968395ef90015eea86f9bb6188e7
Iñaki Abete <inakiabt@gmail.com>
2015-03-06 02:24:11 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,158 @@
+module.exports = function(grunt) {
+    var path = require('path');
+    var appName = "example",
+        moduleDir = grunt.option('module-dir') || '..',
+        workspaceDir = "./tmp",
+        projectDir = workspaceDir + '/' + appName,
+        projectAppifiedDir = projectDir + '/appify',
+        json = grunt.file.readJSON('./package.json');
+
+    grunt.initConfig({
+        pkg: json,
+
+        titanium: {
+            create: {
+                options: {
+                    command: 'create',
+                    name: appName,
+                    projectDir: projectDir,
+                    workspaceDir: workspaceDir
+                }
+            },
+            build: {
+                options: {
+                    command: 'build',
+                    platform: 'ios',
+                    projectDir: projectAppifiedDir
+                }
+            },
+            clean: {
+                options: {
+                    command: 'clean',
+                    projectDir: projectAppifiedDir
+                }
+            }
+        },
+        copy: {
+            specs: {
+                expand: true,
+                src: 'spec/**',
+                dest: projectAppifiedDir
+            },
+            'jasmin-expect': {
+                expand: true,
+                cwd: path.dirname(require.resolve('jasmine-expect')),
+                src: './**',
+                dest: projectAppifiedDir + '/Resources/jasmine-expect'
+            }
+        },
+        mkdir: {
+            appify: {
+                options: {
+                    create: [projectAppifiedDir]
+                }
+            }
+        },
+        unzip: {
+            module: {
+                src: [
+                    'dist/<%= pkg.name %>-commonjs-<%= pkg.version %>.zip'
+                ],
+                dest: projectAppifiedDir
+            }
+        },
+        tishadow: {
+            test: {
+                command: 'spec',
+                options: {
+                    projectDir: projectAppifiedDir
+                }
+            },
+            appify: {
+                command: 'appify',
+                options: {
+                    projectDir: projectDir,
+                    dest: 'appify'
+                }
+            },
+            clear: {
+                command: 'clear',
+                options: {
+                    projectDir: projectAppifiedDir
+                }
+            }
+        },
+        titaniumifier: {
+            build: {
+                files: {
+                    'dist': [moduleDir]
+                },
+                options: {
+                    bundle: true,
+                    module: true
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-titanium');
+    grunt.loadNpmTasks('grunt-tishadow');
+    grunt.loadNpmTasks('grunt-titaniumifier');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-mkdir');
+
+    grunt.registerTask('tiapp:addmodule', function() {
+        var tiapp = require('tiapp.xml').load(projectAppifiedDir + '/tiapp.xml');
+
+        tiapp.setModule(json.name, json.version);
+        tiapp.write();
+    });
+
+    grunt.registerTask('test', [
+        'tishadow:test'
+    ]);
+
+    grunt.registerTask('test:clear', [
+        'tishadow:clear'
+    ]);
+
+    grunt.registerTask('appify', [
+        'mkdir:appify',
+        'tishadow:appify',
+        'unzip:module',
+        'copy:specs',
+        'copy:jasmin-expect',
+        'tiapp:addmodule'
+    ]);
+
+    grunt.registerTask('app:setup', [
+        'titanium:create',
+        'titaniumifier:build',
+        'appify'
+    ]);
+
+    grunt.registerTask('app:build', [
+        'titanium:build'
+    ]);
+
+    grunt.registerTask('clear', [
+        'test:clear',
+        'titanium:clean',
+    ]);
+
+    grunt.registerTask('module:build', [
+        'titaniumifier:build',
+        'unzip:module',
+        'tiapp:addmodule'
+    ]);
+
+    grunt.registerTask('module:setup', [
+        'module:build',
+        'appify',
+        'app:build'
+    ]);
+
+    grunt.registerTask('default', ['app:setup', 'app:build']);
+
+};

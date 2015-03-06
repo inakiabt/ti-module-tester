43a5e5902198ee1b88e9a61d0a60cc9cbd77c735
Iñaki Abete <inakiabt@gmail.com>
2015-03-06 02:58:43 -0300

    Add tmp app id from module name

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
             create: {
                 options: {
                     command: 'create',
+                    id: 'grunttitanium.' + appName.replace('-', ''),
                     name: appName,
                     projectDir: projectDir,
                     workspaceDir: workspaceDir
